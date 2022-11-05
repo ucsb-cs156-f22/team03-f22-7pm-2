57f681529d5acca7305782c43d555efd15bddcdd
DylanC1222 <77405374+DylanC1222@users.noreply.github.com>
2022-11-05 16:07:50 -0700

    dc - Fixed HelpRequests placeholder test

--- frontend/src/tests/pages/HelpRequests/HelpRequestsIndexPage.test.js
+++ frontend/src/tests/pages/HelpRequests/HelpRequestsIndexPage.test.js
@@ -3,6 +3,12 @@ import { QueryClient, QueryClientProvider } from "react-query";
 import { MemoryRouter } from "react-router-dom";
 import HelpRequestsIndexPage from "main/pages/HelpRequests/HelpRequestsIndexPage";
 
+import { apiCurrentUserFixtures } from "fixtures/currentUserFixtures";
+import { systemInfoFixtures } from "fixtures/systemInfoFixtures";
+import axios from "axios";
+import AxiosMockAdapter from "axios-mock-adapter";
+import _mockConsole from "jest-mock-console";
+
 const mockToast = jest.fn();
 jest.mock('react-toastify', () => {
     const originalModule = jest.requireActual('react-toastify');
@@ -21,7 +27,37 @@ jest.mock('react-router-dom', () => ({
 }));
 
 describe("HelpRequestsIndexPage tests", () => {
-    test("renders without crashing", () => {
+
+    const axiosMock =new AxiosMockAdapter(axios);
+
+    const setupUserOnly = () => {
+        axiosMock.reset();
+        axiosMock.resetHistory();
+        axiosMock.onGet("/api/currentUser").reply(200, apiCurrentUserFixtures.userOnly);
+        axiosMock.onGet("/api/systemInfo").reply(200, systemInfoFixtures.showingNeither);
+    };
+
+    const setupAdminUser = () => {
+        axiosMock.reset();
+        axiosMock.resetHistory();
+        axiosMock.onGet("/api/currentUser").reply(200, apiCurrentUserFixtures.adminUser);
+        axiosMock.onGet("/api/systemInfo").reply(200, systemInfoFixtures.showingNeither);
+    };
+    test("renders without crashing for regular user", async () => {
+        setupUserOnly();
+        const queryClient = new QueryClient();
+
+        render(
+            <QueryClientProvider client={queryClient}>
+                <MemoryRouter>
+                    <HelpRequestsIndexPage />
+                </MemoryRouter>
+            </QueryClientProvider>
+        );
+    });
+
+    test("renders without crashing for admin user", () => {
+        setupAdminUser();
         const queryClient = new QueryClient();
 
         render(
