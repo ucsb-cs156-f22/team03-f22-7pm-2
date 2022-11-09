9ce6c1c58fd7cdd3d57fcf20560c3d87ce59374a
EthanEpp <eepp@ucsb.edu>
2022-11-09 02:45:33 -0800

    Added the delete method and functionality

--- frontend/src/main/components/DiningCommonsMenuItem/DiningCommonsMenuItemTable.js
+++ frontend/src/main/components/DiningCommonsMenuItem/DiningCommonsMenuItemTable.js
@@ -1,10 +1,10 @@
-import OurTable from "main/components/OurTable";
-// import { useBackendMutation } from "main/utils/useBackend";
-// import { cellToAxiosParamsDelete, onDeleteSuccess } from "main/utils/UCSBDateUtils"
+import OurTable, { ButtonColumn } from "main/components/OurTable";
+import { useBackendMutation } from "main/utils/useBackend";
+import { cellToAxiosParamsDelete, onDeleteSuccess } from "main/utils/UCSBDateUtils"
 // import { useNavigate } from "react-router-dom";
 import { _hasRole } from "main/utils/currentUser";
 
-export default function DiningCommonsMenuItemTable({ diningCommonsMenuItem, _currentUser }) {
+export default function DiningCommonsMenuItemTable({ diningCommonsMenuItem, currentUser }) {
 
     // const navigate = useNavigate();
 
@@ -13,15 +13,15 @@ export default function DiningCommonsMenuItemTable({ diningCommonsMenuItem, _cur
     // }
 
     // Stryker disable all : hard to test for query caching
-    // const deleteMutation = useBackendMutation(
-    //     cellToAxiosParamsDelete,
-    //     { onSuccess: onDeleteSuccess },
-    //     ["/api/ucsbdates/all"]
-    // );
+    const deleteMutation = useBackendMutation(
+        cellToAxiosParamsDelete,
+        { onSuccess: onDeleteSuccess },
+        ["/api/diningcommonsmenuitem/all"]
+    );
     // Stryker enable all 
 
     // Stryker disable next-line all : TODO try to make a good test for this
-    // const deleteCallback = async (cell) => { deleteMutation.mutate(cell); }
+    const deleteCallback = async (cell) => { deleteMutation.mutate(cell); }
 
     const columns = [
         {
@@ -42,15 +42,15 @@ export default function DiningCommonsMenuItemTable({ diningCommonsMenuItem, _cur
         }
     ];
 
-    // const columnsIfAdmin = [
-    //     ...columns,
-    //     ButtonColumn("Edit", "primary", editCallback, "DiningCommonsMenuItemTable"),
-    //     ButtonColumn("Delete", "danger", deleteCallback, "DiningCommonsMenuItemTable")
-    // ];
+    const columnsIfAdmin = [
+        ...columns,
+        // ButtonColumn("Edit", "primary", editCallback, "DiningCommonsMenuItemTable"),
+        ButtonColumn("Delete", "danger", deleteCallback, "DiningCommonsMenuItemTable")
+    ];
 
-    // const columnsToDisplay = hasRole(currentUser, "ROLE_ADMIN") ? columnsIfAdmin : columns;
+    const columnsToDisplay = hasRole(currentUser, "ROLE_ADMIN") ? columnsIfAdmin : columns;
 
-    const columnsToDisplay = columns;
+    // const columnsToDisplay = columns;
 
 
     return <OurTable
