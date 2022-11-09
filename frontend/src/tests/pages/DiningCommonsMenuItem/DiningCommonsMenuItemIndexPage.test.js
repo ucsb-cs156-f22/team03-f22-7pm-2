cba6ee2425a45b1c024fecebc1e42881a13ce38f
EthanEpp <eepp@ucsb.edu>
2022-11-09 01:44:27 -0800

    Bug fix for unused variable in compile

--- frontend/src/tests/pages/DiningCommonsMenuItem/DiningCommonsMenuItemIndexPage.test.js
+++ frontend/src/tests/pages/DiningCommonsMenuItem/DiningCommonsMenuItemIndexPage.test.js
@@ -33,12 +33,12 @@ describe("DiningCommonsMenuItemIndexPage tests", () => {
 
     const axiosMock =new AxiosMockAdapter(axios);
 
-    // const setupUserOnly = () => {
-    //     axiosMock.reset();
-    //     axiosMock.resetHistory();
-    //     axiosMock.onGet("/api/currentUser").reply(200, apiCurrentUserFixtures.userOnly);
-    //     axiosMock.onGet("/api/systemInfo").reply(200, systemInfoFixtures.showingNeither);
-    // };
+    const setupUserOnly = () => {
+        axiosMock.reset();
+        axiosMock.resetHistory();
+        axiosMock.onGet("/api/currentUser").reply(200, apiCurrentUserFixtures.userOnly);
+        axiosMock.onGet("/api/systemInfo").reply(200, systemInfoFixtures.showingNeither);
+    };
 
     const setupAdminUser = () => {
         axiosMock.reset();
